8769c04e245bb3412a1a54c746e3ce192b807b4c
MassouAnas <massouanas3@gmail.com>
2024-05-08 00:36:21 +0100

    Chi router and middleware added

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", ourBasicHandler)
+	
+
+	server := &http.Server{
+		Addr: ":3333",
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to listen to server", err)
+	}
+}
+
+func ourBasicHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello, world!"))
 }
